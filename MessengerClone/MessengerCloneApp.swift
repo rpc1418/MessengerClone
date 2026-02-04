fd3a94f184dcd0cfd32833bd6839bd238c152b19
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-02-03 23:51:11 -0800

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by rentamac on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
