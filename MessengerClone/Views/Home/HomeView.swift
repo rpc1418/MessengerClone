bf05ce8b1ade7d33937288fc6e3db27ccb6d137b
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-02-05 00:19:53 -0800

    Rudraksh-Changes to home branch

--- MessengerClone/Views/Home/HomeView.swift
+++ MessengerClone/Views/Home/HomeView.swift
@@ -5,3 +5,15 @@
 //  Created by rentamac on 03/02/2026.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
