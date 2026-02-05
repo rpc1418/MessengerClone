0208f08e064ee4a37faaadf45e10f41d35400cf2
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-02-05 00:23:31 -0800

    Rudraksh - Changes to login view in auth branch

--- MessengerClone/Views/Auth/LoginView.swift
+++ MessengerClone/Views/Auth/LoginView.swift
@@ -5,3 +5,14 @@
 //  Created by rentamac on 03/02/2026.
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login View")
+    }
+}
+
+#Preview {
+    LoginView()
+}
