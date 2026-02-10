6c12f34db65a35f175e278d0119a5098563720be
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-02-09 20:34:48 -0800

    Rudraksh - Commiting unwanted changes

--- MessengerClone/Views/Home/HomeView.swift
+++ MessengerClone/Views/Home/HomeView.swift
@@ -4,20 +4,3 @@
 //
 //  Created by rentamac on 03/02/2026.
 //
-
-import SwiftUI
-
-struct Home: View {
-    @EnvironmentObject var authViewModel: AuthViewModel
-    @EnvironmentObject var appRouter: AppRouter
-    var body: some View {
-        Text("Hello, World!")
-        Text("HomeView")
-        Button("Sign Out!!!"){
-            authViewModel.signOut()
-        }
-        Button("Go To Chat"){
-            appRouter.navigate(to: .NewChatViewNav)
-        }
-    }
-}
